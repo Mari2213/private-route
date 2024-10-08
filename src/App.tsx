60f007b097d2a46c2bb09b5d350e7e45196ae353
Mari2213 <mariannasilvapereira15@gmail.com>
2024-10-08 19:10:00 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import NavBar from "./components/NavBar.tsx";
+import { RequireAuth } from "./contexts/Auth/RequireAuth.tsx";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </>
   );
