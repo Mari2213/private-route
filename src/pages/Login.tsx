2b935dd3f021b1ad844336a2e8fa8f2089369014
Mari2213 <mariannasilvapereira15@gmail.com>
2024-10-08 19:12:46 -0300

    feat: add states for inpunts

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,46 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
-    <form className="mt-5 d-flex align-items-center justify-content-center flex-column">
-      <div className="row mb-3">
-        <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">
-          Email
-        </label>
-        <div className="col-auto">
-          <input type="email" className="form-control" id="inputEmail3" />
+    <form className="mt-5 d-flex align-items-center justify-content-center">
+      <div className="justify-content-between d-flex flex-column">
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">
+            Email
+          </label>
+          <div className="col-auto ms-4">
+            <input
+              type="email"
+              value={email}
+              className="form-control"
+              id="inputEmail3"
+            />
+          </div>
         </div>
-      </div>
-      <div className="row mb-3">
-        <label htmlFor="inputPassword3" className="col-sm-3 col-form-label">
-          Password
-        </label>
-        <div className="col-auto">
-          <input type="password" className="form-control" id="inputPassword3" />
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-3 col-form-label">
+            Password
+          </label>
+          <div className="col-auto">
+            <input
+              type="password"
+              value={password}
+              className="form-control"
+              id="inputPassword3"
+            />
+          </div>
         </div>
+        <button onClick={handleLogin} type="submit" className="btn btn-primary">
+          Sign in
+        </button>
       </div>
-      <button type="submit" className="btn btn-primary">
-        Sign in
-      </button>
     </form>
   );
 };
